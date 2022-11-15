c0db0c17371aa1e975e2df4e363985c2311d41b2
Gerbo645 <akarimgerbo11@gmail.com>
2022-11-15 21:11:38 +0100

    première version

---
+++ script.js
@@ -0,0 +1,40 @@
+//let divp2=document.getElementById("divp2")
+//divp2.parentNode.style.backgroundColor="yellow"
+/*
+let div=document.querySelector("div")
+let enfant=div.children
+
+for(el of enfant){
+    el.style.backgroundColor="red"
+    
+}
+//enfant[0].style.backgroundColor="red"
+*/
+//let div = document.createElement("div")
+//let p = document.createElement("p")
+//p.textContent = "Abalo va à l ecole"
+//div.append(p)
+//document.body.prepend(div) 
+
+/*
+let div=document.createElement("div")
+div.setAttribute('class','divs')
+let ul = document.createElement("ul")
+let li1 = document.createElement("li")
+li1.id='1'
+li1.textContent="premier"
+let li2 = document.createElement("li")
+li2.id='2'
+li2.textContent="deuxieme"
+ul.append(li1)
+ul.append(li2)
+let p = document.createElement("p")
+p.className='paras'
+p.id='paras'
+p.textContent='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ipsam sunt voluptatem, quo voluptatum quos recusandae illum similique. Officiis expedita ipsam, a praesentium architecto omnis eaque ipsum illo facilis sint laborum! Dicta odit qui alias vitae iure iste officiis reiciendis amet eaque molestiae voluptate, voluptates esse eum asperiores quibusdam dolor.'
+div.append(ul)
+div.append(p)
+document.body.append(div)
+*/
+
+divs.addEventListener
