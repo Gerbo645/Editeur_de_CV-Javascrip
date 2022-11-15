901aa7c50f5e390a38a5a97155ca55c29cb8f129
Gerbo645 <akarimgerbo11@gmail.com>
2022-11-15 21:18:11 +0100

    deuxième version

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
-//let divp2=document.getElementById("divp2")
-//divp2.parentNode.style.backgroundColor="yellow"
-/*
-let div=document.querySelector("div")
-let enfant=div.children
 
-for(el of enfant){
-    el.style.backgroundColor="red"
-    
-}
-//enfant[0].style.backgroundColor="red"
-*/
-//let div = document.createElement("div")
-//let p = document.createElement("p")
-//p.textContent = "Abalo va à l ecole"
-//div.append(p)
-//document.body.prepend(div) 
-
-/*
-let div=document.createElement("div")
-div.setAttribute('class','divs')
-let ul = document.createElement("ul")
-let li1 = document.createElement("li")
-li1.id='1'
-li1.textContent="premier"
-let li2 = document.createElement("li")
-li2.id='2'
-li2.textContent="deuxieme"
-ul.append(li1)
-ul.append(li2)
-let p = document.createElement("p")
-p.className='paras'
-p.id='paras'
-p.textContent='Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ipsam sunt voluptatem, quo voluptatum quos recusandae illum similique. Officiis expedita ipsam, a praesentium architecto omnis eaque ipsum illo facilis sint laborum! Dicta odit qui alias vitae iure iste officiis reiciendis amet eaque molestiae voluptate, voluptates esse eum asperiores quibusdam dolor.'
-div.append(ul)
-div.append(p)
-document.body.append(div)
-*/
-
-divs.addEventListener
+let name_input = document.getElementById('name_input')
+name_input.addEventListener('input',(e)=>{
+    name_output.textContent=e.target.value
+})
+let prenom_input = document.getElementById('prenom_input')
+prenom_input.addEventListener('input',(e)=>{
+    prenom_output.textContent=e.target.value
+})
+let sexe_input = document.getElementById('sexe_input')
+sexe_input.addEventListener('input',(e)=>{
+    sexe_output.textContent=e.target.value
+})
+let naiss_input = document.getElementById('naiss_input')
+naiss_input.addEventListener('input',(e)=>{
+    naiss_output.textContent=e.target.value
+})
+let libelle1 = document.getElementById('libelle1_input')
+libelle1_input.addEventListener('input',(e)=>{
+    libelle1_output.textContent=e.target.value
+})
+let libelle2 = document.getElementById('libelle2_input')
+libelle2_input.addEventListener('input',(e)=>{
+    libelle2_output.textContent=e.target.value
+})
+let libelle3 = document.getElementById('libelle3_input')
+libelle3_input.addEventListener('input',(e)=>{
+    libelle3_output.textContent=e.target.value
+})
+let debut1 = document.getElementById('debut1_input')
+debut1_input.addEventListener('input',(e)=>{
+    debut1_output.textContent=e.target.value
+})
+let debut2 = document.getElementById('debut2_input')
+debut2_input.addEventListener('input',(e)=>{
+    debut2_output.textContent=e.target.value
+})
+let debut3 = document.getElementById('debut3_input')
+debut3_input.addEventListener('input',(e)=>{
+    debut3_output.textContent=e.target.value
+})
+let datefin1 = document.getElementById('fin1_input')
+fin1_input.addEventListener('input',(e)=>{
+    fin1_output.textContent=e.target.value
+})
+let datefin2 = document.getElementById('fin2_input')
+fin2_input.addEventListener('input',(e)=>{
+    fin2_output.textContent=e.target.value
+})
+let datefin3 = document.getElementById('fin3_input')
+fin3_input.addEventListener('input',(e)=>{
+    fin3_output.textContent=e.target.value
+})
+let contact_input = document.getElementById('contact_input')
+contact_input.addEventListener('input',(e)=>{
+    contact_output.textContent=e.target.value
+})
+let adresse_input = document.getElementById('adresse_input')
+adresse_input.addEventListener('input',(e)=>{
+    adresse_output.textContent=e.target.value
+})
+let email_input = document.getElementById('email_input')
+email_input.addEventListener('input',(e)=>{
+    email_output.textContent=e.target.value
+})
+let parcour_input = document.getElementById('parcour_input')
+parcour_input.addEventListener('input',(e)=>{
+    parcour_output.textContent=e.target.value
+})
